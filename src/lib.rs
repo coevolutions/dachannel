bc87febd3bb46c18675bc2e7c366b451b82eec35
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-02-27 19:36:51 -0800

    add dachannel test

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,72 @@ pub mod channel;
 pub mod connection;
 
 pub use channel::Channel;
-pub use connection::Connection;
+pub use connection::*;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+        }
+    }
+
+    #[cfg_attr(not(target_arch = "wasm32"), pollster::test)]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+    pub async fn test_connection_new() {
+        let conn = Connection::new(Default::default()).unwrap();
+        conn.create_data_channel("test", Default::default())
+            .unwrap();
+    }
+
+    #[cfg_attr(not(target_arch = "wasm32"), pollster::test)]
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+    pub async fn test_connection_communicate() {
+        let conn1 = Connection::new(Default::default()).unwrap();
+        let chan1 = conn1
+            .create_data_channel(
+                "test",
+                DataChannelOptions {
+                    negotiated: true,
+                    id: Some(1),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+        conn1.set_local_description(SdpType::Offer).await.unwrap();
+        while conn1.next_ice_gathering_state().await.unwrap() != IceGatheringState::Complete {}
+
+        let conn2 = Connection::new(Default::default()).unwrap();
+        let chan2 = conn2
+            .create_data_channel(
+                "test",
+                DataChannelOptions {
+                    negotiated: true,
+                    id: Some(1),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+        conn2
+            .set_remote_description(&conn1.local_description().unwrap().unwrap())
+            .await
+            .unwrap();
+
+        conn2.set_local_description(SdpType::Answer).await.unwrap();
+        while conn2.next_ice_gathering_state().await.unwrap() != IceGatheringState::Complete {}
+
+        conn1
+            .set_remote_description(&conn2.local_description().unwrap().unwrap())
+            .await
+            .unwrap();
+
+        chan1.send(b"hello world!").await.unwrap();
+        assert_eq!(chan2.recv().await.unwrap(), b"hello world!");
+
+        chan2.send(b"goodbye world!").await.unwrap();
+        assert_eq!(chan1.recv().await.unwrap(), b"goodbye world!");
+    }
+}

91e59db72d4208a0054ad8c364f64802cc7acd13
coevolutions <156261562+coevolutions@users.noreply.github.com>
2024-02-27 22:15:55 -0800

    simplify server implementation

--- src/server.rs
+++ src/server.rs
@@ -2,29 +2,74 @@ use datachannel_facade::platform::native::ConfigurationExt as _;
 use http_body_util::BodyExt as _;
 use hyper::body::Body as _;
 
-pub struct Server<T> {
-    conn_rx: async_channel::Receiver<(crate::Connection, T)>,
+pub struct Server {
+    connecting_rx: async_channel::Receiver<Connecting>,
 }
 
-async fn offer<T>(
-    conn_tx: async_channel::Sender<(crate::Connection, T)>,
-    initialize_conn: impl Fn(
-            &hyper::HeaderMap,
-            &crate::Connection,
-        ) -> Result<T, Box<dyn std::error::Error + Send + Sync>>
-        + Send
-        + Sync
-        + Clone
-        + 'static,
+struct ConnectingInner {
+    connection: crate::Connection,
+    body: hyper::body::Incoming,
+    answer_sdp_tx: tokio::sync::oneshot::Sender<Option<String>>,
+}
+
+pub struct Connecting {
+    headers: hyper::HeaderMap,
+    inner: Option<ConnectingInner>,
+}
+
+impl Connecting {
+    pub fn connection(&self) -> &crate::Connection {
+        &self.inner.as_ref().unwrap().connection
+    }
+
+    pub fn headers(&self) -> &hyper::HeaderMap {
+        &self.headers
+    }
+
+    pub async fn finish(
+        mut self,
+    ) -> Result<crate::Connection, Box<dyn std::error::Error + Send + Sync>> {
+        let inner = self.inner.take().unwrap();
+
+        let offer_sdp = String::from_utf8(inner.body.collect().await?.to_bytes().to_vec())?;
+
+        inner
+            .connection
+            .set_remote_description(&crate::Description {
+                type_: crate::SdpType::Offer,
+                sdp: offer_sdp,
+            })
+            .await?;
+        inner
+            .connection
+            .set_local_description(crate::SdpType::Answer)
+            .await?;
+        inner.connection.ice_candidates_gathered().await;
+
+        // TODO: Remove this unwrap?
+        let answer_sdp = inner.connection.local_description()?.unwrap().sdp;
+        let _ = inner.answer_sdp_tx.send(Some(answer_sdp));
+
+        Ok(inner.connection)
+    }
+}
+
+impl Drop for Connecting {
+    fn drop(&mut self) {
+        if let Some(inner) = self.inner.take() {
+            let _ = inner.answer_sdp_tx.send(None);
+        }
+    }
+}
+
+async fn offer(
+    connecting_tx: async_channel::Sender<Connecting>,
     bind_addr: std::net::SocketAddr,
     req: hyper::Request<hyper::body::Incoming>,
 ) -> Result<
     hyper::Response<http_body_util::Full<hyper::body::Bytes>>,
     Box<dyn std::error::Error + Send + Sync>,
->
-where
-    T: Send + Sync + 'static,
-{
+> {
     if req.method() != hyper::Method::POST {
         let mut resp = hyper::Response::new(http_body_util::Full::new(hyper::body::Bytes::from(
             hyper::StatusCode::METHOD_NOT_ALLOWED
@@ -42,8 +87,6 @@ where
 
     let conn = crate::Connection::new(config)?;
 
-    let r = initialize_conn(&parts.headers, &conn)?;
-
     if body.size_hint().upper().unwrap_or(u64::MAX) > 8 * 1024 {
         let mut resp = hyper::Response::new(http_body_util::Full::new(hyper::body::Bytes::from(
             hyper::StatusCode::PAYLOAD_TOO_LARGE
@@ -54,45 +97,43 @@ where
         return Ok(resp);
     }
 
-    let offer_sdp = String::from_utf8(body.collect().await?.to_bytes().to_vec())?;
+    let (answer_sdp_tx, answer_sdp_rx) = tokio::sync::oneshot::channel();
+    connecting_tx
+        .send(Connecting {
+            inner: Some(ConnectingInner {
+                connection: conn,
+                body,
+                answer_sdp_tx,
+            }),
+            headers: parts.headers,
+        })
+        .await?;
 
-    conn.set_remote_description(&crate::Description {
-        type_: crate::SdpType::Offer,
-        sdp: offer_sdp,
-    })
-    .await?;
-    conn.set_local_description(crate::SdpType::Answer).await?;
-    conn.ice_candidates_gathered().await;
-
-    // TODO: Remove this unwrap?
-    let answer_sdp = conn.local_description()?.unwrap().sdp;
-
-    let _ = conn_tx.send((conn, r)).await;
+    let answer_sdp = if let Some(answer_sdp) = answer_sdp_rx.await? {
+        answer_sdp
+    } else {
+        let mut resp = hyper::Response::new(http_body_util::Full::new(hyper::body::Bytes::from(
+            hyper::StatusCode::FORBIDDEN
+                .canonical_reason()
+                .unwrap_or(""),
+        )));
+        *resp.status_mut() = hyper::StatusCode::FORBIDDEN;
+        return Ok(resp);
+    };
 
     Ok(hyper::Response::new(http_body_util::Full::new(
         hyper::body::Bytes::from(answer_sdp),
     )))
 }
 
-impl<T> Server<T>
-where
-    T: Send + Sync + 'static,
-{
+impl Server {
     pub async fn listen(
-        addr: std::net::SocketAddr,
-        initialize_conn: impl Fn(
-                &hyper::HeaderMap,
-                &crate::Connection,
-            ) -> Result<T, Box<dyn std::error::Error + Send + Sync>>
-            + Send
-            + Sync
-            + Clone
-            + 'static,
+        listener: tokio::net::TcpListener,
         backlog: usize,
     ) -> Result<Self, std::io::Error> {
-        let listener = tokio::net::TcpListener::bind(addr.clone()).await?;
+        let bind_addr = listener.local_addr()?;
 
-        let (conn_tx, conn_rx) = async_channel::bounded(backlog);
+        let (connecting_tx, connecting_rx) = async_channel::bounded(backlog);
         tokio::task::spawn(async move {
             loop {
                 let (stream, _) = match listener.accept().await {
@@ -104,16 +145,15 @@ where
                 };
                 let io = hyper_util::rt::TokioIo::new(stream);
 
-                let bind_addr = addr.clone();
-                let conn_tx = conn_tx.clone();
-                let initialize_conn = initialize_conn.clone();
+                let bind_addr = bind_addr.clone();
+                let connecting_tx = connecting_tx.clone();
 
                 tokio::task::spawn(async move {
                     if let Err(err) = hyper::server::conn::http1::Builder::new()
                         .serve_connection(
                             io,
                             hyper::service::service_fn(move |req| {
-                                offer::<T>(conn_tx.clone(), initialize_conn.clone(), bind_addr, req)
+                                offer(connecting_tx.clone(), bind_addr, req)
                             }),
                         )
                         .await
@@ -124,10 +164,10 @@ where
             }
         });
 
-        Ok(Self { conn_rx })
+        Ok(Self { connecting_rx })
     }
 
-    pub async fn accept(&self) -> Option<(crate::Connection, T)> {
-        self.conn_rx.recv().await.ok()
+    pub async fn accept(&self) -> Option<Connecting> {
+        self.connecting_rx.recv().await.ok()
     }
 }
